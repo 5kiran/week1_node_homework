cab04e7e0fe8a21ea981ac7d2c9f88bc26d0894e
OHGILHWAN <dhrlfghks01@gmail.com>
2022-12-18 19:39:31 +0900

    fix : 게시글 삭제해도 댓글이 남아있는 버그 수정

--- routes/posts.js
+++ routes/posts.js
@@ -20,7 +20,12 @@ router.post("/", async (req,res) => {
 // 게시글 목록 API
 router.get("/", async (req,res) => {
   const posts = await Posts.find({},{'password':false,'__v':false,'updatedAt':false}).sort({createdAt:-1});
-  res.json({posts});
+  if (!posts.length){
+    res.status(400).json({'message':'게시물이 존재하지 않습니다'})
+  }
+  else{
+    res.json({posts});
+  }
 })
 
 
@@ -32,7 +37,10 @@ router.get("/:_id", async (req,res) => {
     let comments = await Comments.find({postid:_id},{'_id':false,'password':false,'__v':false,'updatedAt':false}).sort({createdAt:-1});
     let postData = {};
     postData.post = result
-    postData.comments = comments
+    if (comments.length){
+      postData.comments = comments
+    }
+    
     res.json(postData)
   }
   catch (err) {
@@ -79,6 +87,7 @@ router.delete("/:_id", async (req,res) => {
     };
     if (find[0].password === password){
       await Posts.deleteOne({_id});
+      await Comments.deleteMany({postid : _id})
       res.json({"message": "게시글을 삭제하였습니다.."})
     }else{
       res.status(403).json({"message": "비밀번호가 틀렸습니다."})
